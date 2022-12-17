e4a5af2357c5f081c6a1e3bbc9a022b589bc6567
vatsal <vatsalsinghkv@gmail.com>
2022-12-17 22:26:06 +0530

    v1.0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content: ['./index.html'],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Montserrat', 'sans-serif'],
+    },
+
+    extend: {
+      colors: {
+        text: '#4E5150',
+        muted: '#A9A9A9',
+        'light-gray': '#E5E5E5',
+      },
+      fontSize: {
+        title: '0.875rem', // 14px
+        primary: '0.75rem', // 12px
+        secondary: '0.625rem', // 10px
+        'title-lg': '1.5rem', // 24px
+        'primary-lg': '1.125rem', // 18px
+        'secondary-lg': '0.875rem', // 14px
+      },
+      screens: {
+        xs: '375px',
+      },
+      fontWeight: {
+        normal: 600,
+      },
+    },
   },
   plugins: [],
 };
