3da6d5beae0b3c585955b33b66fd7757b8e42a5a
vatsal <vatsalsinghkv@gmail.com>
2022-12-17 22:48:53 +0530

    v1.1

---
+++ src/script.js
@@ -0,0 +1,28 @@
+const addressEl = document.getElementById('address');
+const nameEl = document.getElementById('name');
+
+const persons = [
+  {
+    name: 'Vedika Kamane',
+    address: 'Pune, Maharashtra',
+  },
+  {
+    name: 'Anagha Avinash',
+    address: 'Banglore, Karanataka',
+  },
+  {
+    name: 'Nikhat Faridous',
+    address: 'Hazaribagh, Jharkhand',
+  },
+  {
+    name: 'Manisha Bajpai',
+    address: 'Noida, Uttar Pradesh',
+  },
+];
+
+const getRandomPerson = () => persons[Math.floor(Math.random() * (persons.length - 1))];
+
+const person = getRandomPerson();
+
+nameEl.textContent = person.name;
+addressEl.textContent = person.address;
